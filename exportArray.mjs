fbce1488847b24bdc3c2cfb6071dcb95859a9e59
naveen <v.naveen38@gmail.com>
2022-05-10 20:09:52 +0000

    import and export of an array

---
+++ exportArray.mjs
@@ -0,0 +1,3 @@
+let arrayEx = ["countries", 190, "continents", 7, false, 6.2];
+
+export default arrayEx;
---
+++ importArray.mjs
@@ -0,0 +1,8 @@
+import arrayEx from "./exportArray.mjs";
+
+console.log(arrayEx[0]);
+console.log(arrayEx[1]);
+console.log(arrayEx[2]);
+console.log(arrayEx[3]);
+console.log(arrayEx[4]);
+console.log(arrayEx[5]);
